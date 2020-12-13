a0083c47516ffa8e9999c37d3bc17c49b8d514b7
Globa <Globa@DESKTOP-E96IGNL>
2020-12-13 14:50:32 +0300

    Интегрирование | Воробьёв Д,Ю. | ДВ-21

---
+++ Интегрирование/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Интегрирование
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Functions = new System.Windows.Forms.ComboBox();
+            this.FunctionIMG = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Info = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Error = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.FunctionImages = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.FunctionIMG)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Functions
+            // 
+            this.Functions.FormattingEnabled = true;
+            this.Functions.Items.AddRange(new object[] {
+            "Функция №1",
+            "Функция №2",
+            "Функция №3",
+            "Функция №4",
+            "Функция №5"});
+            this.Functions.Location = new System.Drawing.Point(105, 40);
+            this.Functions.Name = "Functions";
+            this.Functions.Size = new System.Drawing.Size(153, 21);
+            this.Functions.TabIndex = 0;
+            this.Functions.SelectedIndexChanged += new System.EventHandler(this.Functions_SelectedIndexChanged);
+            // 
+            // FunctionIMG
+            // 
+            this.FunctionIMG.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.FunctionIMG.InitialImage = null;
+            this.FunctionIMG.Location = new System.Drawing.Point(105, 67);
+            this.FunctionIMG.Name = "FunctionIMG";
+            this.FunctionIMG.Size = new System.Drawing.Size(153, 35);
+            this.FunctionIMG.TabIndex = 1;
+            this.FunctionIMG.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(92, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберите функцию:";
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(105, 37);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(153, 20);
+            this.Result.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Info);
+            this.panel1.Controls.Add(this.Functions);
+            this.panel1.Controls.Add(this.FunctionIMG);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(3);
+            this.panel1.Size = new System.Drawing.Size(360, 166);
+            this.panel1.TabIndex = 4;
+            // 
+            // Info
+            // 
+            this.Info.Location = new System.Drawing.Point(105, 108);
+            this.Info.Multiline = true;
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(153, 47);
+            this.Info.TabIndex = 3;
+            this.Info.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(132, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Результат:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(118, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Погрешность:";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.Error);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.Result);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(12, 184);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(3);
+            this.panel2.Size = new System.Drawing.Size(360, 133);
+            this.panel2.TabIndex = 5;
+            // 
+            // Error
+            // 
+            this.Error.Location = new System.Drawing.Point(105, 95);
+            this.Error.Name = "Error";
+            this.Error.Size = new System.Drawing.Size(153, 20);
+            this.Error.TabIndex = 6;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(12, 323);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(360, 36);
+            this.Calculate.TabIndex = 6;
+            this.Calculate.Text = "Рассчитать";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // FunctionImages
+            // 
+            this.FunctionImages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("FunctionImages.ImageStream")));
+            this.FunctionImages.TransparentColor = System.Drawing.Color.Transparent;
+            this.FunctionImages.Images.SetKeyName(0, "Function 1.PNG");
+            this.FunctionImages.Images.SetKeyName(1, "Function 2.PNG");
+            this.FunctionImages.Images.SetKeyName(2, "Function 3.PNG");
+            this.FunctionImages.Images.SetKeyName(3, "Function 4.PNG");
+            this.FunctionImages.Images.SetKeyName(4, "Function 5.PNG");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 369);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Интегрирование";
+            ((System.ComponentModel.ISupportInitialize)(this.FunctionIMG)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Functions;
+        private System.Windows.Forms.PictureBox FunctionIMG;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox Info;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox Error;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.ImageList FunctionImages;
+    }
+}
+
---
+++ Интегрирование/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Интегрирование
+{
+    public partial class Form1 : Form
+    {
+        private const double h = 0.05f; 
+
+        private Func<double, double> function;
+        private Func<double, double> derFunction;
+        private double a, b;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Functions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Info.Clear();
+            int selectedFunction = Functions.SelectedIndex;
+            switch(selectedFunction)
+            {
+                case 0:
+                    SetFunction(Function_1);
+                    SetTheSecondDerivative(DerFunction_1);
+                    SetFunctionInfo(1.5,2.5);
+                    SetImageOfFunction(0);
+                    break;
+                case 1:
+                    SetFunction(Function_2);
+                    SetTheSecondDerivative(DerFunction_2);
+                    SetFunctionInfo(-1.4, -0.4);
+                    SetImageOfFunction(1);
+                    break;
+                case 2:
+                    SetFunction(Function_3);
+                    SetTheSecondDerivative(DerFunction_3);
+                    SetFunctionInfo(3, 4);
+                    SetImageOfFunction(2);
+                    break;
+                case 3:
+                    SetFunction(Function_4);
+                    SetTheSecondDerivative(DerFunction_4);
+                    SetFunctionInfo(1, 2);
+                    SetImageOfFunction(3);
+                    break;
+                case 4:
+                    SetFunction(Function_5);
+                    SetTheSecondDerivative(DerFunction_5);
+                    SetFunctionInfo(-2.5, -1.5);
+                    SetImageOfFunction(4);
+                    break;
+            }
+            Result.Clear();
+            Error.Clear();
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            double integral = FindIntegral();
+            double error = FindError();
+            Result.Text = Math.Round(integral, 3).ToString();
+            Error.Text = Math.Round(error, 5).ToString();
+        }
+
+        private double FindIntegral()
+        {
+            double alpha = a + h / 2;
+            double n = (b - a) / h;
+            double result = 0;
+            double func = 0;
+
+            for (double i = 0; i < n; i += 1)
+            {
+                func = function(alpha + i * h);
+                result += func;
+                if (Math.Abs(func) < 0.1 * Math.Pow(10, 1 - i)) break;
+            }
+
+            return result * h;
+        }
+
+        private double FindError()
+        {
+            double n = (b - a) / h;
+            double maxDer = FindMaxDer(a,b);
+            return Math.Pow(b - a, 3) * Math.Abs(maxDer) / (24 * Math.Pow(n, 2));
+        }
+
+        private double FindMaxDer(double min, double max)
+        {
+            double maxVal = double.NegativeInfinity;
+            for (double i = min; i <= max; i += 0.1)
+            {
+                double func = derFunction(i);
+                if (func > maxVal) maxVal = func;
+            }
+            return maxVal;
+        }
+
+        private void SetFunction(Func<double, double> function)
+        {
+            this.function = function;
+        }
+
+        private void SetTheSecondDerivative(Func<double, double> derivative)
+        {
+            derFunction = derivative;
+        }
+
+        private void SetFunctionInfo(double a, double b)
+        {
+            this.a = a; this.b = b;
+            AddLine(Info, $"A = {a}");
+            AddLine(Info, $"B = {b}");
+            AddLine(Info, $"H = {h}");
+        }
+
+        private void SetImageOfFunction(int index)
+        {
+            FunctionIMG.Image = FunctionImages.Images[index];
+        }
+
+        private void AddLine(TextBox textBox, string line)
+        {
+            textBox.Text += line;
+            textBox.Text += "\r\n";
+        }
+
+        private double Function_1(double x)
+        {
+            return 1 + Math.Sin(4 * x) / Math.Log(x);
+        }
+
+        private double DerFunction_1(double x)
+        {
+            return -(16 * Math.Sin(4 * x) / Math.Log(x)) - (8 * Math.Cos(4 * x) / (x * Math.Pow(Math.Log(x), 2))) + (Math.Sin(4 * x) / (Math.Pow(x, 2) * Math.Pow(Math.Log(x), 2))) + (2 * Math.Sin(4 * x) / (Math.Pow(x, 2) * Math.Pow(Math.Log(x), 3)));
+        }
+
+        private double Function_2(double x)
+        {
+            return -10 * Math.Sin(Math.Pow(x, 3)) * Math.Cos(-x);
+        }
+
+        private double DerFunction_2(double x)
+        {
+            return 90 * Math.Pow(x, 4) * Math.Sin(Math.Pow(x, 3)) * Math.Cos(x) + 60 * Math.Pow(x, 2) * Math.Sin(Math.Pow(x, 3)) - 60 * x * Math.Cos(x) * Math.Sin(Math.Pow(x, 3)) + 10 * Math.Cos(x) * Math.Sin(Math.Pow(x, 3));
+        }
+
+        private double Function_3(double x)
+        {
+            return Math.Exp(x) - 4 * Math.Exp(-x) - 1;
+        }
+
+        private double DerFunction_3(double x)
+        {
+            return (Math.Exp(2 * x) - 4) / Math.Exp(x);
+        }
+
+        private double Function_4(double x)
+        {
+            return Math.Cos(x) - Math.Sqrt(x + 2) + 1;
+        }
+
+        private double DerFunction_4(double x)
+        {
+            return -Math.Cos(x) + 1 / (4 * Math.Sqrt(x + 2) * (x + 2));
+        }
+
+        private double Function_5(double x)
+        {
+            return Math.Cos(1 + 0.2 * Math.Pow(x, 2)) - x;
+        }
+
+        private double DerFunction_5(double x)
+        {
+            return (2 * Math.Sin(1+0.2*Math.Pow(x,2)) / 5) + (4 * Math.Pow(x,2) * Math.Cos(1 + 0.2 * Math.Pow(x,2)) / 25);
+        }
+    }
+}
